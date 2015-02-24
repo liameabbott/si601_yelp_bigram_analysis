41408225752dad699b5f57f0f1a11abfdaa90265
Liam Abbott <leabbott@umich.edu>
2015-02-24 12:54:24 -0500

    update

---
+++ si601_project_analysis_leabbott.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+
+import re
+from itertools import groupby
+from getngrams import runQuery as rq
+import csv
+import math 
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+def bigram_counter(reviews):
+    """Function takes as input a list whose elements are the text of individual 
+       business reviews and returns a list of lists, where each sub-list
+       contains a bigram and the number of times that bigram was used in the
+       reviews. The master list is sorted by the number of times each bigram
+       was used, in descending order."""
+       
+    word_pattern = re.compile(r"\b[\w]+\b")
+    
+    #Create list of all bigrams in the list of reviews.
+    bigram_list = []
+    for review in reviews:
+        words = word_pattern.findall(review)
+        for i in range(len(words)-1):
+            bigram = words[i].lower() + ' ' + words[i+1].lower()
+            bigram_list.append(bigram)
+    
+    #Sort the bigram list alphabetically.
+    sorted_bigram_list = sorted(bigram_list)
+    
+    #Using itertools groupby() function, count occurences of each bigram in the
+    #sorted bigram list created above.
+    bigram_counts_list = []
+    for key, group in groupby(sorted_bigram_list, lambda x: x):
+        bigram_count = 0
+        for bigram in group:
+            bigram_count = bigram_count + 1
+        bigram_counts_list.append([key, bigram_count])
+        
+    bigram_counts_list_sorted = sorted(bigram_counts_list, 
+                                       key = lambda x: -x[1])
+    
+    #Return the sorted list of bigrams and their respective counts.
+    return bigram_counts_list_sorted
+
+   
+#Open the .txt files containg the positive reviews.
+with open('pos_reviews.txt', 'rU') as f:
+    pos_reviews = f.readlines()
+
+#Open the .txt files containg the negative reviews.    
+with open('neg_reviews.txt', 'rU') as f:
+    neg_reviews = f.readlines()
+
+#Create sorted lists of positive and negative bigrams and their counts.
+pos_bgs_cts = bigram_counter(pos_reviews)
+neg_bgs_cts = bigram_counter(neg_reviews)
+
+#Count the total number of positive bigrams (not unqiue).
+n_pos_bgs = 0.0
+for i in range(len(pos_bgs_cts)):
+    n_pos_bgs += float(pos_bgs_cts[i][1])
+
+#Count the total number of negative bigrams (not unique).
+n_neg_bgs = 0.0
+for i in range(len(neg_bgs_cts)):
+    n_neg_bgs += float(neg_bgs_cts[i][1])    
+
+#Calculate the probabilities of all positive bigrams by calculating the total
+#number of occurrences of each bigram divided by the total number of bigrams in
+#all positive reviews. Do the same for negative bigrams.
+pos_bg_probs = [float(bg[1])/n_pos_bgs for bg in pos_bgs_cts]
+neg_bg_probs = [float(bg[1])/n_neg_bgs for bg in neg_bgs_cts]
+
+#Create list of lists where each sub-list contains a bigram, the number of
+#occurrences of that bigram in positive reviews, and the probability of
+#occurrence for that bigram. Sorted by most commonly occurring.
+pos_bgs_cts_probs = []
+for i in range(len(pos_bgs_cts)):
+    bg = pos_bgs_cts[i][0]
+    ct = pos_bgs_cts[i][1]
+    prob = pos_bg_probs[i]
+    pos_bgs_cts_probs.append([bg, ct, prob])
+
+#Create list of lists where each sub-list contains a bigram, the number of
+#occurrences of that bigram in negative reviews, and the probability of
+#occurrence for that bigram. Sorted by most commonly occurring.
+neg_bgs_cts_probs = []
+for i in range(len(neg_bgs_cts)):
+    bg = neg_bgs_cts[i][0]
+    ct = neg_bgs_cts[i][1]
+    prob = neg_bg_probs[i]
+    neg_bgs_cts_probs.append([bg, ct, prob])   
+
+#Create a tab-delimited .txt file with list of bigrams, counts, probabilities
+#found in positive reviews, sorted by most frequently occuring. 
+with open('pos_bgs_cts_probs.txt', 'wb') as f:
+    for line in pos_bgs_cts_probs:
+        f.write(line[0] + '\t' + str(line[1]) + '\t' + str(line[2]) + '\n')
+
+#Create a tab-delimited .txt file with list of bigrams, counts, probabilities
+#found in negative reviews, sorted by most frequently occuring.         
+with open('neg_bgs_cts_probs.txt', 'wb') as f:
+    for line in neg_bgs_cts_probs:
+        f.write(line[0] + '\t' + str(line[1]) + '\t' + str(line[2]) + '\n')
+
+#Set parameters for call to runQuery method in getngrams.py script
+params = """ --startYear=2007 --endYear=2008 --corpus=eng_us_2012 --smoothing=3
+             --caseInsensitive --noprint"""
+
+#Set positive bigram and negative bigram queries to send to runQuery method in
+#getngrams.py script.
+pos_queries = ['the best', 'the food', 'the bar', 'i love', 'my favorite',
+               'the service', 'the menu', 'happy hour', 'the staff',
+               'great place']
+          
+neg_queries = ['the food', 'the bar', 'the service', 'no one', 'the manager',
+                'the waitress', 'the bartender', 'my husband', 'it took',
+                'the drinks']
+
+#Call runQuery method on positive bigrams and negative bigrams.
+pos_res = rq(', '.join(pos_queries) + params)
+neg_res = rq(', '.join(neg_queries) + params)
+
+#Open .csv files created by runQuery method and extract words and probability
+#information for positive and negative bigrams.               
+param_file_handle = '-eng_us_2012-2007-2008-3-caseInsensitive.csv'
+pos_file_handle = '_'.join(pos_queries).replace(' ','')
+neg_file_handle = '_'.join(neg_queries).replace(' ','')
+
+pos_filename = pos_file_handle + param_file_handle
+neg_filename = neg_file_handle + param_file_handle
+
+with open(pos_filename, 'rU') as f:
+    l = [line for line in csv.reader(f, delimiter='\t')]
+    pos_words = l[0][0]
+    pos_probs = l[1][0]
+
+with open(neg_filename, 'rU') as f:
+    l = [line for line in csv.reader(f, delimiter='\t')]
+    neg_words = l[0][0]
+    neg_probs = l[1][0]
+
+pos_eng_words = [word for word in pos_words.split(',')[1:]]
+pos_eng_probs = [float(num) for num in pos_probs.split(',')[1:]]
+
+neg_eng_words = [word for word in neg_words.split(',')[1:]]
+neg_eng_probs = [float(num) for num in neg_probs.split(',')[1:]]
+
+#Extract positive review probability of selected bigrams.
+pos_rev_probs = []
+for i in range(len(pos_bgs_cts_probs)):
+    for word in pos_eng_words:
+        if word == pos_bgs_cts_probs[i][0]:
+            pos_rev_probs.append(pos_bgs_cts_probs[i][2])
+
+#Extract negative review probability of selected bigrams.
+neg_rev_probs = []
+for i in range(len(neg_bgs_cts_probs)):
+    for word in neg_eng_words:
+        if word == pos_bgs_cts_probs[i][0]:
+            neg_rev_probs.append(neg_bgs_cts_probs[i][2])
+
+#Take the log of the ratio of positive review probability to English
+#language probability for selected positive bigrams.
+pos_log_ratios = [math.log(pos_rev_probs[i]/pos_eng_probs[i]) 
+                    for i in range(len(pos_rev_probs))]
+
+#Take the log of the ratio of negative review probability to English
+#language probability for selected negative bigrams.                        
+neg_log_ratios = [math.log(neg_rev_probs[i]/neg_eng_probs[i])
+                    for i in range(len(neg_rev_probs))]
+
+#Create and save bar plots of log-probabilities for positive and negative
+#bigrams.
+n_groups = 10
+index = np.arange(n_groups)
+bar_width = 0.35
+opacity = 0.4
+
+plt.figure()
+pos_labels = [word.replace(' ','\n') for word in pos_eng_words]                     
+pos_bar = plt.bar(index, pos_log_ratios, bar_width, alpha = opacity,
+                  color = 'r')
+pos_line = plt.axhline(y=1.0, color = 'k', lw = 2)
+plt.title('Log of Prob(Pos Rev)/Prob(Eng Lang)')
+plt.xticks(index+(bar_width/2), pos_labels)
+plt.tight_layout()
+plt.savefig('pos_probs_chart.pdf', bbox_inches='tight')
+
+plt.figure()
+neg_labels = [word.replace(' ','\n') for word in neg_eng_words]                 
+neg_bar = plt.bar(index, neg_log_ratios, bar_width, alpha = opacity,
+                  color = 'b')
+beg_line = plt.axhline(y=1.0, color = 'k', lw = 2)
+plt.title('Log of Prob(Neg Rev)/Prob(Eng Lang)')
+plt.xticks(index+(bar_width/2), neg_labels)
+plt.tight_layout()
+plt.savefig('neg_probs_chart.pdf', bbox_inches='tight')
+
+
+
+
+
+
+
+
+
---
+++ si601_project_preprocess_leabbott.py
@@ -0,0 +1,94 @@
+
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+
+import json
+import sqlite3 as sqlite
+
+
+def bar_business_generator():
+    """Generator function that opens Yelp Academic 'business' dataset and 
+       yields (business id, avg star rating) tuple for every business in the 
+       dataset categorized as a bar.""" 
+    
+    with open('yelp_academic_dataset_business.json', 'rU') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            line_dict = json.loads(line)
+            if 'Bars' in line_dict['categories']:
+                bar_id = line_dict['business_id']
+                avg_star_rating = line_dict['stars']
+                yield (bar_id, avg_star_rating)
+                
+                
+def review_generator():
+    """Generator function that opens Yelp Academic 'reviews' dataset and yields
+       (business id, star rating, review text) tuple for every review in the 
+       dataset. Dataset does not include category of businesses reviewed, so
+       we cannot filter out the reviews of only bars yet!"""    
+    
+    with open('yelp_academic_dataset_review.json', 'rU') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            line_dict = json.loads(line)
+            biz_id = line_dict['business_id']
+            stars = line_dict['stars']
+            review = line_dict['text']
+            yield (biz_id, stars, review)
+    
+    
+#Create sqlite database on disk to hold and combine data from disparate Yelp
+#business and reviews datasets. 
+with sqlite.connect('si601_project_database_leabbott.db') as con:
+    
+    cur = con.cursor()
+    
+    #Create business table in database.
+    cur.execute("DROP TABLE IF EXISTS business")
+    cur.execute("CREATE TABLE business(id TEXT, star_rating REAL)")     
+    
+    #Create reviews table in database.
+    cur.execute("DROP TABLE IF EXISTS reviews")
+    cur.execute("CREATE TABLE reviews(id TEXT, stars REAL, review TEXT)")    
+    
+    #Write (business id, avg star rating) for all bars into a table.
+    bars = bar_business_generator()    
+    cur.executemany("INSERT INTO business VALUES(?, ?)", bars)
+    
+    #Write (business id, star rating, review text) for all reviews into table.
+    reviews = review_generator()
+    cur.executemany("INSERT INTO reviews VALUES(?, ?, ?)", reviews)
+    
+    #Create indices for each table based on business id
+    cur.execute("CREATE INDEX IF NOT EXISTS index_business_id ON business(id)")    
+    cur.execute("CREATE INDEX IF NOT EXISTS index_reviews_id ON reviews(id)")    
+    
+    #Join business and reviews tables on business id. Select star rating and
+    #review text for all reviews of businesses categorized as bars in the
+    #business dataset. Write (star rating, review text) tuples into a list.
+    cur.execute("""SELECT r.stars, r.review
+                    FROM reviews AS r JOIN business AS b ON (r.id=b.id)""")
+    bar_reviews = cur.fetchall()
+    
+    #Create positive and negative review lists, where positive reviews are all
+    #5-star reviews and negative reviews are all reviews with 2 or fewer stars.
+    pos_reviews = [review[1].encode('utf8').replace('\n','\s') for review in bar_reviews
+                                        if review[0] == 5.0]
+    neg_reviews = [review[1].encode('utf8').replace('\n','\s') for review in bar_reviews
+                                        if review[0] <= 2.0]
+    
+    #Write positive reviews list to a .txt file.
+    with open('pos_reviews.txt', 'wb') as f:
+        for line in pos_reviews:
+            f.write(line + '\n')
+            
+    #Write negative reviews list to a .txt file.
+    with open('neg_reviews.txt', 'wb') as f:
+        for line in neg_reviews:
+            f.write(line + '\n')
+                    
